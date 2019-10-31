458e4df154d553c5fbadbf9bbf3befe13158de51
Jacykow <jacgulij@gmail.com>
2019-10-31 18:15:40 +0100

    init

---
+++ Bike.cs
@@ -0,0 +1,29 @@
+﻿namespace AkademiaTest
+{
+    public class Bike
+    {
+        public float distance;
+        public string type;
+
+        public Bike(string type)
+        {
+            this.type = type;
+            distance = 0;
+        }
+
+        public Bike()
+        {
+            distance = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Rower {type} jest w odległości {distance}m od domu.";
+        }
+
+        public void Move(float distanceDelta)
+        {
+            this.distance += distanceDelta;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AkademiaTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            /* 
+            Wszystkie nazwy klas, metod i pól są po angielsku, 
+            ponieważ w tym języku pomagają na stackoverflow
+            */
+            var bikeA = new Bike("szybki");
+            var bikeB = new Bike("składak")
+            {
+                distance = 2.5f
+            };
+
+            var bikes = new List<Bike>
+            {
+                bikeA,
+                bikeB,
+                new Bike("zwykły")
+            };
+
+            foreach (var rower in bikes)
+            {
+                rower.Move(rower.type == "szybki" ? 2.5f : 1.0f);
+            }
+
+            var bikeD = new Bike("bmx");
+            bikes.Add(bikeD);
+            bikeD.distance = 1.5f;
+
+            foreach (var bike in bikes)
+            {
+                Console.WriteLine(bike);
+            }
+
+            // Ta metoda poczeka na wprowadzenie dowolnego znaku
+            Console.ReadKey();
+        }
+    }
+}
